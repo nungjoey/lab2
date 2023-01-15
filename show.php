e69751c3ad98533cbfe6bebad63233225748eeb2
กิตติพงศ์ เวียงวิเศษ <kittipong.wai@ku.th>
2023-01-15 18:07:27 +0700

    changs mr

--- show.php
+++ show.php
@@ -3,11 +3,13 @@
 
 <body>
     <?php
+    $Mr./Ms./Miss/Mrs. = $_POST["Mr./Ms./Miss/Mrs."]
     $name = $_POST["name"];
     $age = $_POST["age"];
     $sex = $_POST["sex"];
     $status = $_POST["status"];
 
+    echo "Mr./Ms./Miss/Mrs." . $Mr./Ms./Miss/Mrs. . "<br>";
     echo "Name:" . $name . "<br>";
     echo "Age:" . $age . "<br>";
     echo "Sex:" . $sex . "<br>";
