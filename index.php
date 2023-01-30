fe6687b258f3fb73cd302819b6e12267e9dededd
Kittipong <106949714+nungjoey@users.noreply.github.com>
2023-01-30 10:57:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,12 +3,15 @@
 <body>
 
 <form action="show.php" method="post">
-    what is your MRorMS:<input type="text" name="MRorMS"><br>
-    what is your name:<input type="text" name="name"><br>
-    what is your age:<input type="text" name="age"><br>
-    what is your sex:<input type="text" name="sex"><br>
-    what is your status:<input type="text" name="status"><br>
-    <input type="submit" value="Submit">
+        your name:<input type="text" name="name">.<br>
+        your age:<input type="text" name="age">.<br>
+        your gender:
+        <input type="radio" name="gender" value="female">woman
+        <input type="radio" name="gender" value="male">man<br>
+        your status:
+        <input type="radio" name="status" value="unmarried">unmarried
+        <input type="radio" name="status" value="married">married<br>
+        <input type="submit" value="Submit"><br>
 </form>
 
 </body>
