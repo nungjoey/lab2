dbc9cea4ff0c5e943149b61b9645e789f8b9f14d
Kittipong <106949714+nungjoey@users.noreply.github.com>
2023-01-30 10:58:48 +0700

    Update show.php

--- show.php
+++ show.php
@@ -3,18 +3,48 @@
 
 <body>
     <?php
-    $MRorMS = $_POST["MRorMS"];
-    $name = $_POST["name"];
-    $age = $_POST["age"];
-    $sex = $_POST["sex"];
-    $status = $_POST["status"];
-
-    echo "MRorMS" . $MRorMS . "<br>";
-    echo "Name:" . $name . "<br>";
-    echo "Age:" . $age . "<br>";
-    echo "Sex:" . $sex . "<br>";
-    echo "Status:" . $status . "<br>";
-
+    $name =$_GET["name"];
+    $age=$_GET["age"];
+    $gender=$_GET["gender"];
+    $status=$_GET["status"];
+    if ($age < "15" && $gender == "male" && $status == "unmarried"){
+        $titaltxt="DC.";
+        $gender = "Male";
+        $status="unmarried";
+    }else if($age > "14" && $gender == "male" && $status == "unmarried"){
+        $titaltxt="Mr.";
+        $gender = "Male";
+        $status="unmarried";
+    }else if($age > "14" && $gender == "male" && $status == "married"){
+        $titaltxt="Mr.";
+        $gender = "Male";
+        $status="unmarried";
+    }else if($age < "15" && $gender == "male" && $status == "married"){
+        $titaltxt="DC.";
+        $gender = "Male";
+        $status="unmarried";
+    }else if($age < "15" && $gender == "female" && $status == "unmarried"){
+        $titaltxt="girl.";
+        $gender = "female";
+        $status="unmarried";
+    }else if($age > "14" && $gender == "female" && $status == "unmarried"){
+        $titaltxt="ms.";
+        $gender = "female";
+        $status="unmarried";
+    }else if($age > "14" && $gender == "female" && $status == "married"){
+        $titaltxt="Mrs.";
+        $gender = "female";
+        $status="married";
+    }else if($age < "15" && $gender == "female" && $status == "married"){
+        $titaltxt="girl.";
+        $gender = "female";
+        $status="married";
+    }
+    echo "<font style='font-size:24px'>your information</font>"."<br>";
+    echo "name:".$titaltxt.$name."<br>";
+    echo "age:".$age."<br>";
+    echo "sex:".$gender."<br>";
+    echo "status:".$status."<br>";
     ?>
 
 </body>
